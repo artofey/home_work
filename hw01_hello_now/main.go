db75c94089cd2182624e08d09e3ae634c80d8d9a
Артем Яровенко <iarovenko@fintech.ru>
2020-08-29 13:52:52 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const (
+	ntpHost = "0.beevik-ntp.pool.ntp.org"
+)
+
 func main() {
-	// Place your code here
+	beevikTime, err := ntp.Time(ntpHost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("current time: %v\n", time.Now().Round(0))
+	log.Printf("exact time: %v\n", beevikTime.Round(0))
 }
