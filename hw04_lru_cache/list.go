685a221b3f8f7ae85569f9cbeb963b5958f7475c
Yarovenko Artyom <artik3386@gmail.com>
2020-09-28 12:05:29 +0300

    п

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,17 +1,62 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+// List is ...
 type List interface {
-	// Place your code here
+	Len() int
+	Front() *ListItem
+	Back() *ListItem
+	PushFront(v interface{}) *ListItem
+	PushBack(v interface{}) *ListItem
+	Remove(i *ListItem)
+	MoveToFront(i *ListItem)
 }
 
-type listItem struct {
-	// Place your code here
+type ListItem struct {
+	Value interface{}
+	Next  *ListItem
+	Prev  *ListItem
 }
 
 type list struct {
-	// Place your code here
+	ListItem
 }
 
+// Длина списка
+func (list) Len() int {
+	return 0
+}
+
+// Первый элемент списка
+func (list) Front() *ListItem {
+	return nil
+}
+
+// Последний элемент списка
+func (list) Back() *ListItem {
+	return nil
+}
+
+// Добавить значение в начало
+func (list) PushFront(v interface{}) *ListItem {
+	return nil
+}
+
+// Добавить значение в конец
+func (list) PushBack(v interface{}) *ListItem {
+	return nil
+}
+
+// Удалить элемент
+func (list) Remove(i *ListItem) {
+
+}
+
+// Переместить элемент в начало
+func (list) MoveToFront(i *ListItem) {
+
+}
+
+// NewList is ...
 func NewList() List {
 	return &list{}
 }
