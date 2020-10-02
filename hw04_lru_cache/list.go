2ad43e63e443ca045ce68eaf8d13b178592f9928
Yarovenko Artyom <artik3386@gmail.com>
2020-10-02 23:04:53 +0300

    Сделал часть методов

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -18,32 +18,58 @@ type ListItem struct {
 }
 
 type list struct {
-	l []ListItem
+	count int
+	first *ListItem
+	last  *ListItem
 }
 
 // Длина списка
-func (list) Len() int {
-	return 0
+func (l *list) Len() int {
+	return l.count
 }
 
 // Первый элемент списка
-func (list) Front() *ListItem {
-	return nil
+func (l *list) Front() *ListItem {
+	return l.first
 }
 
 // Последний элемент списка
-func (list) Back() *ListItem {
-	return nil
+func (l *list) Back() *ListItem {
+	return l.last
 }
 
 // Добавить значение в начало
-func (l list) PushFront(v interface{}) *ListItem {
-	return nil
+func (l *list) PushFront(v interface{}) *ListItem {
+	newListItem := ListItem{Value: v}
+	if l.count == 0 {
+		l.first = &newListItem
+		l.last = &newListItem
+		l.count++
+		return &newListItem
+	}
+	l.first.Prev = &newListItem
+
+	newListItem.Next = l.first
+	l.first = &newListItem
+	l.count++
+	return &newListItem
 }
 
 // Добавить значение в конец
-func (l list) PushBack(v interface{}) *ListItem {
-	return nil
+func (l *list) PushBack(v interface{}) *ListItem {
+	newListItem := ListItem{Value: v}
+	if l.count == 0 {
+		l.first = &newListItem
+		l.last = &newListItem
+		l.count++
+		return &newListItem
+	}
+	l.last.Next = &newListItem
+
+	newListItem.Prev = l.last
+	l.last = &newListItem
+	l.count++
+	return &newListItem
 }
 
 // Удалить элемент
