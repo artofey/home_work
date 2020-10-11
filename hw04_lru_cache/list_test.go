1ed13a494109ffbbfd385c54526a6d26a5a24bdb
Yarovenko Artem <artik3386@gmail.com>
2020-10-11 12:32:30 +0300

    добавил много тестов чтобы поймать ошибки

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -42,16 +42,138 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
-		elems := make([]int, 0, l.Len())
+		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{50, 30, 10, 40, 60, 80, 70}, MakeNativeIntList(l, true))
+	})
+
+	t.Run("push front", func(t *testing.T) {
+		l := NewList()
+
+		item20 := l.PushFront(20)
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, item20, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.Equal(t, []int{20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20}, MakeNativeIntList(l, true))
+
+		item40 := l.PushFront(40)
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, item40, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.Equal(t, []int{40, 20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20, 40}, MakeNativeIntList(l, true))
+
+		item10 := l.PushFront(10)
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, item10, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.NotEqual(t, item40, l.Back())
+		require.NotEqual(t, item40, l.Front())
+		require.Equal(t, []int{10, 40, 20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20, 40, 10}, MakeNativeIntList(l, true))
+	})
+
+	t.Run("push back", func(t *testing.T) {
+		l := NewList()
+
+		item20 := l.PushBack(20) // 20
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, item20, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.Empty(t, item20.Prev)
+		require.Empty(t, item20.Next)
+		require.Equal(t, []int{20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20}, MakeNativeIntList(l, true))
+
+		item40 := l.PushBack(40) // 20, 40
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, item20, l.Front())
+		require.Equal(t, item40, l.Back())
+		require.Equal(t, item20.Next, item40)
+		require.Equal(t, item40.Prev, item20)
+
+		require.Equal(t, []int{20, 40}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{40, 20}, MakeNativeIntList(l, true))
+
+		item10 := l.PushBack(10) // 20, 40, 10
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, item20, l.Front())
+		require.Equal(t, item10, l.Back())
+		require.NotEqual(t, item40, l.Back())
+		require.NotEqual(t, item40, l.Front())
+		require.Equal(t, item20.Next, item40)
+		require.Equal(t, item40.Prev, item20)
+		require.Equal(t, item40.Next, item10)
+		require.Equal(t, item10.Prev, item40)
+		require.Equal(t, []int{20, 40, 10}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{10, 40, 20}, MakeNativeIntList(l, true))
+	})
+
+	t.Run("move to front", func(t *testing.T) {
+		l := NewList()
+
+		item20 := l.PushFront(20) // 20
+		l.MoveToFront(item20)     // 20
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, item20, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.Equal(t, []int{20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20}, MakeNativeIntList(l, true))
+
+		item30 := l.PushFront(30) // 30, 20
+		l.MoveToFront(item30)     // 30, 20
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, item30, l.Front())
+		require.Equal(t, item20, l.Back())
+		require.Equal(t, []int{30, 20}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{20, 30}, MakeNativeIntList(l, true))
+
+		l.MoveToFront(item20) // 20, 30
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, item20.Value, l.Front().Value)
+		require.Equal(t, item30.Value, l.Back().Value)
+		require.Equal(t, item20.Next, item30)
+		require.Equal(t, item30.Prev, item20)
+		require.Equal(t, []int{20, 30}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{30, 20}, MakeNativeIntList(l, true))
+
+		item40 := l.PushBack(40) // 20, 30, 40
+		l.MoveToFront(item40)    // 40, 20, 30
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, item40.Value, l.Front().Value)
+		require.Equal(t, item30.Value, l.Back().Value)
+
+		require.Equal(t, item40.Next, item20)
+		require.Equal(t, item20.Prev, item40)
+		require.Equal(t, item20.Next, item30)
+		require.Equal(t, item30.Prev, item20)
+		require.Equal(t, []int{40, 20, 30}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{30, 20, 40}, MakeNativeIntList(l, true))
+
+		l.MoveToFront(item20) // 20, 40, 30
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, item20.Value, l.Front().Value)
+		require.Equal(t, item30.Value, l.Back().Value)
+
+		require.Equal(t, item20.Next, item40)
+		require.Equal(t, item40.Prev, item20)
+		require.Equal(t, item40.Next, item30)
+		require.Equal(t, item30.Prev, item40)
+		require.Equal(t, []int{20, 40, 30}, MakeNativeIntList(l, false))
+		require.Equal(t, []int{30, 40, 20}, MakeNativeIntList(l, true))
+	})
+}
+
+func MakeNativeIntList(l List, revert bool) []int {
+	elems := make([]int, 0, l.Len())
+	if revert == false {
 		for i := l.Front(); i != nil; i = i.Next {
 			elems = append(elems, i.Value.(int))
 		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
-		
-		elems = make([]int, 0, l.Len())
+	} else {
 		for i := l.Back(); i != nil; i = i.Prev {
 			elems = append(elems, i.Value.(int))
 		}
-		require.Equal(t, []int{50, 30, 10, 40, 60, 80, 70}, elems)
-	})
+	}
+	return elems
 }
