7db9ba8239953ec3203d85aabc37ee330af3bf26
Yarovenko Artem <artik3386@gmail.com>
2020-10-18 15:04:22 +0300

    Done

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,21 +9,55 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks.
-func Run(tasks []Task, N int, M int) error {
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+func Run(tasks []Task, n int, m int) error {
 	wg := sync.WaitGroup{}
-	for _, task := range tasks {
-		task = task
+	doneCh := make(chan interface{})
+	tasksCh := make(chan Task, len(tasks))
+	errorsCh := make(chan error)
+	moreError := false
+
+	for i := 0; i < n; i++ {
 		wg.Add(1)
-		mu := sync.Mutex{}
 		go func() {
-			mu.Lock()
-			defer mu.Unlock()
-			task()
+			worker(doneCh, tasksCh, errorsCh)
 			wg.Done()
 		}()
 	}
 
+	for _, task := range tasks {
+		tasksCh <- task
+	}
+	close(tasksCh)
+
+	var errors int
+	for i := 0; i < len(tasks); i++ {
+		err := <-errorsCh
+		if err != nil {
+			errors++
+			if errors >= m {
+				close(doneCh)
+				moreError = true
+				break
+			}
+		}
+	}
+
 	wg.Wait()
+	close(errorsCh)
+	if moreError {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func worker(done <-chan interface{}, tasks <-chan Task, errors chan<- error) {
+	for task := range tasks {
+		err := task()
+		select {
+		case <-done:
+			return
+		case errors <- err:
+		}
+	}
 }
