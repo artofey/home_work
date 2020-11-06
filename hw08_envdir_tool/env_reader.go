63318638e8ab722be6fee284dfaa12048d5a6d6a
Артем Яровенко <iarovenko@fintech.ru>
2020-11-06 17:42:03 +0300

    starting

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +15,40 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func getEnvValue(fileName string) (string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	fmt.Println(fileName, scanner.Text())
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	return "", nil
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envs := make(Environment)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		envVal, err := getEnvValue(dir + "/" + file.Name())
+		if err != nil {
+			return nil, err
+		}
+		envs[file.Name()] = envVal
+	}
+	return envs, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	input    string
+	expected Environment
+	err      interface{}
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	testCases := []testCase{
+		{
+			input:    "testdata/nodir",
+			expected: nil,
+			err:      &os.PathError{},
+		},
+		{
+			input: "testdata/env",
+			expected: Environment{
+				"BAR":   "bar",
+				"HELLO": "hello",
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.input, func(t *testing.T) {
+			res, err := ReadDir(test.input)
+
+			require.Equal(t, test.expected, res)
+			require.IsType(t, test.err, err)
+		})
+	}
 }
