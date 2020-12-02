1bdbe817a5bcb4c2191ff477633f274fca01a728
artofey <artik3386@gmail.com>
2020-12-02 16:39:56 +0300

    ниче не работает

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,44 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	host, port, address string
+	timeout             int64
+)
+
+func init() {
+	flag.Int64Var(&timeout, "timeout", 10, "connection timeout")
+	flag.Parse()
+
+	host = flag.Arg(0)
+	if flag.Arg(1) == "" {
+		port = "23"
+	} else {
+		port = flag.Arg(1)
+	}
+	address = net.JoinHostPort(host, port)
+}
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	client := NewTelnetClient(address, time.Duration(timeout), os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	time.Sleep(1 * time.Minute)
+	fmt.Println("Exita app.")
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,18 +1,109 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
+// TelnetClient ...
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Close() error
+	Send() error
+	Receive() error
 }
 
+// NewTelnetClient create new instance telnet client.
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+		dealer:  net.Dialer{},
+	}
+}
+
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	dealer  net.Dialer
+	ctx     context.Context
+	conn    net.Conn
+	cancel  context.CancelFunc
+}
+
+func (c *telnetClient) Connect() error {
+	c.ctx, c.cancel = context.WithTimeout(
+		context.Background(),
+		time.Duration(timeout)*time.Second,
+	)
+
+	var err error
+	c.conn, err = c.dealer.DialContext(c.ctx, "tcp", c.address)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here
-// P.S. Author's solution takes no more than 50 lines
+func (c *telnetClient) Close() error {
+	c.cancel()
+	return nil
+}
+
+func (c *telnetClient) Send() error {
+	return nil
+}
+
+func (c *telnetClient) Receive() error {
+	return nil
+}
+
+func (c *telnetClient) readRoutine() {
+	scanner := bufio.NewScanner(c.conn)
+OUTER:
+	for {
+		select {
+		case <-c.ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				log.Printf("CANNOT SCAN")
+				break OUTER
+			}
+			text := scanner.Text()
+			log.Printf("From server: %s", text)
+		}
+	}
+	log.Printf("Finished readRoutine")
+}
+
+func (c *telnetClient) writeRoutine() {
+	scanner := bufio.NewScanner(c.in)
+OUTER:
+	for {
+		select {
+		case <-c.ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				break OUTER
+			}
+			str := scanner.Text()
+			log.Printf("To server %v\n", str)
+
+			c.conn.Write([]byte(fmt.Sprintf("%s\n", str)))
+		}
+
+	}
+	log.Printf("Finished writeRoutine")
+}
