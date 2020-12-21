09da9b0e948e46b901c59309f359c15f45833ebe
Yarovenko Artem <artik3386@gmail.com>
2020-12-22 01:15:55 +0300

    начал хранилку

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,48 @@
 package storage
 
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	ErrDateBusy = fmt.Errorf("Данное время уже занято другим событием.")
+)
+
+type IDT int64
+
+type EventsStorage interface {
+	AddEvent(e Event) error
+	UpdateEvent(id IDT, e Event) error
+	DeleteEvent(id IDT) error
+	ShowDayEvents(dt time.Time) error
+	ShowWeekEvents(dt time.Time) error
+	ShowMonthEvents(dt time.Time) error
+}
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID               IDT
+	Title            string
+	DateTime         time.Time
+	TimeDuration     time.Duration
+	Description      string
+	UserID           IDT
+	NotificationTime time.Time
+}
+
+func (e *Event) AddDescription(d string) {
+	e.Description = d
+}
+
+func (e *Event) SetNotificationTime(t time.Time) {
+	e.NotificationTime = t
+}
+
+func NewEvent(
+	title string,
+	dateTime time.Time,
+	timeDuration time.Duration,
+	userID IDT,
+) Event {
+
 }
--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,13 +1,48 @@
 package memorystorage
 
-import "sync"
+import (
+	"sync"
+	"time"
+
+	st "github.com/artofey/home_work/hw12_13_14_15_calendar/internal/storage"
+)
 
 type Storage struct {
 	// TODO
 	mu sync.RWMutex
+	ev st.Event
 }
 
-func New() *Storage {
+func (s Storage) AddEvent(e st.Event) error {
+	return nil
+}
+
+func (s Storage) UpdateEvent(id st.IDT, e st.Event) error {
+	return nil
+
+}
+
+func (s Storage) DeleteEvent(id st.IDT) error {
+	return nil
+
+}
+
+func (s Storage) ShowDayEvents(dt time.Time) error {
+	return nil
+
+}
+
+func (s Storage) ShowWeekEvents(dt time.Time) error {
+	return nil
+
+}
+
+func (s Storage) ShowMonthEvents(dt time.Time) error {
+	return nil
+
+}
+
+func New() st.EventsStorage {
 	return &Storage{}
 }
 
