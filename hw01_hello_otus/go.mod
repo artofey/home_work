62291de54d79f419c57851de5091d7b1d361bcba
Yarovenko Artem <artik3386@gmail.com>
2021-02-25 13:09:40 +0300

    переделал первую домашку

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -2,7 +2,4 @@ module github.com/artofey/home_work/hw01_hello_otus
 
 go 1.16
 
-require (
-	github.com/beevik/ntp v0.3.0
-	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
-)
+require golang.org/x/example v0.0.0-20210113200257-bcf50bfd7dcd
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,21 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 
-	"github.com/beevik/ntp"
+	"golang.org/x/example/stringutil"
 )
 
 const (
-	ntpHost = "0.beevik-ntp.pool.ntp.org"
+	hello = "Hello, OTUS!"
 )
 
 func main() {
-	beevikTime, err := ntp.Time(ntpHost)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("current time: %v\n", time.Now().Round(0))
-	fmt.Printf("exact time: %v\n", beevikTime.Round(0))
+	fmt.Println(stringutil.Reverse(hello))
 }
